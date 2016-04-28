15142c29f59a555ed6d2cd866fa75453610628fe
ib222dp <ib222dp@student.lnu.se>
2016-04-28 22:44:23 +0200

    Started creating PMThread form handling

--- src/AppBundle/Controller/PMThreadController.php
+++ src/AppBundle/Controller/PMThreadController.php
@@ -3,7 +3,13 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use \DateTime;
+use AppBundle\Entity\PrivateMessage;
+use AppBundle\Entity\PMThreadParticipation;
+use AppBundle\Entity\PMThread;
+use AppBundle\Form\Type\PMThreadType;
 
 class PMThreadController extends Controller
 {
@@ -14,22 +20,26 @@ class PMThreadController extends Controller
     {
         if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
             $loggedInUser = $this->getDoctrine()->getRepository('AppBundle:AppUser')->find($this->getUser()->getId());
-            $createdPMThreads = $loggedInUser->getCreatedPMThreads()->toArray();
-            $PMThreads = $loggedInUser->getPMThreads()->toArray();
-            $threads = array_merge($createdPMThreads, $PMThreads);
+            $participations = $loggedInUser->getPMThreadParticipations()->toArray();
 
             $newArray = array();
 
-            foreach ($threads as $thread)
+            foreach ($participations as $participation)
             {
-                $PMs = $thread->getPMs();
-                usort($PMs, function($a, $b)
+                $pType = $participation->getParticipationType();
+
+                if($pType === 'Active')
                 {
-                    return $b->getCreationDate()->format('U') - $a->getCreationDate()->format('U');
-                });
-                foreach ($PMs as $PM) {
-                    array_push($newArray, $PM);
-                    break;
+                    $thread = $participation->getPMThread();
+                    $PMs = $thread->getPMs();
+                    usort($PMs, function($a, $b)
+                    {
+                        return $b->getCreationDate()->format('U') - $a->getCreationDate()->format('U');
+                    });
+                    foreach ($PMs as $PM) {
+                        array_push($newArray, $PM);
+                        break;
+                    }
                 }
             }
 
@@ -46,11 +56,72 @@ class PMThreadController extends Controller
     /**
      * @Route("/pmthreads/create", name="pmthread_create")
      */
-    public function createAction()
+    public function createAction(Request $request)
     {
         if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY'))
         {
+            $em = $this->getDoctrine()->getManager();
+            $appUser = $em->getRepository('AppBundle:AppUser')->find($this->getUser()->getId());
 
+            $friendArray = array();
+
+            foreach ($appUser->getFriendships() as $friendship)
+            {
+                if ($friendship->getFriendshipType()->getFshipType() == "Accepted")
+                {
+                   array_push($friendArray, $friendship->getFriendUser());
+                }
+            }
+
+            $form = $this->createForm(
+                new PMThreadType($friendArray),
+                array('action' => $this->generateUrl('pmthread_create'))
+            );
+
+            $form->handleRequest($request);
+
+            if ($form->isValid())
+            {
+                $message = $form['message']->getData();
+                $recipients = $form['recipients']->getData();
+
+                $PMThread = new PMThread();
+                $PMThread->setCreator($appUser);
+
+                $PM = new PrivateMessage();
+                $PM->setMessage($message);
+                $PM->setCreationDate(new DateTime());
+                $PM->setPMThread($PMThread);
+                $PM->setCreator($appUser);
+                $PM->setRecipients($recipients);
+
+                array_unshift($recipients, $appUser);
+                $participations = array();
+                foreach($recipients as $recipient)
+                {
+                    $participation = new PMThreadParticipation();
+                    $participation->setAppUser($recipient);
+                    $participation->setPMThread($PMThread);
+                    $participation->setParticipationType($em->getRepository('AppBundle:PMThreadParticipationType')->find(1));
+                    $em->persist($participation);
+                    array_push($participations, $participation);
+                }
+                $PMThread->setParticipations($participations);
+                $PMArray = array();
+                array_push($PMArray, $PM);
+                $PMThread->setPMs($PMArray);
+
+                $em->persist($PMThread);
+                $em->persist($PM);
+                $em->flush();
+
+                return $this->redirectToRoute('pmthreads');
+            }
+
+            return $this->render(
+                'PMThread/register.html.twig',
+                array('form' => $form->createView())
+            );
         }
         else
         {
---
+++ src/AppBundle/Form/Type/PMThreadType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\ChoiceList\ObjectChoiceList;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository;
+
+class PMThreadType extends AbstractType
+{
+    private $friendArray;
+    private $choiceList;
+
+    public function __construct($friendArray)
+    {
+        $this->friendArray = $friendArray;
+        $this->choiceList = new ObjectChoiceList($friendArray, 'username', array(), null, 'id');
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $friendArray = $this->friendArray;
+        $builder->add('message', 'textarea');
+        $builder->add('recipients', 'choice', array(
+            'choice_list' => $this->choiceList,
+            'choices' => $friendArray,
+            'multiple' => 'true'
+        ));
+
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+
+    }
+
+    public function getName()
+    {
+        return 'PMThread';
+    }
+
+}
